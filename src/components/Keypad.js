be1eed62daa62418ca5b391279dbbac527e080e9
SamuelOchieng <o.samuelyannick@gmail.com>
2025-01-19 08:39:57 +0300

    Successful

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,11 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <label htmlFor="pass">Password</label>
+            <input id="pass" type="password" placeholder="password" name="inputpassword"
+            onChange={() => console.log("Entering password...")} />
+        </div>
     )
 }
 
